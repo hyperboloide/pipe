ec23d676c1e0700dae5acf780ad471fab5aabe59
Frederic Delbos <fred.delbos@gmail.com>
2016-10-27 00:43:57 +0200

    s3 test

--- rw/s3/s3_test.go
+++ rw/s3/s3_test.go
@@ -10,14 +10,22 @@ import (
 
 func TestS3(t *testing.T) {
 
-	if os.Getenv("AWS_ACCESS_KEY_ID") == "" || os.Getenv("AWS_SECRET_ACCESS_KEY") == "" {
-		log.Print("AWS_ACCESS env variable not found! Skipping TestS3")
-		return
+	if os.Getenv("AWS_ACCESS_KEY_ID") == "" {
+		log.Fatal("AWS_ACCESS_KEY_ID  env variable not found! Skipping TestS3")
+
+	} else if os.Getenv("AWS_SECRET_ACCESS_KEY") == "" {
+		log.Fatal("AWS_SECRET_ACCESS_KEY  env variable not found! Skipping TestS3")
 	}
 
 	s := &s3.S3{
-		Domain: "s3-eu-central-1.amazonaws.com",
-		Bucket: "test.pipe",
+		Bucket:    "test.pipe",
+		Domain:    "s3-eu-central-1.amazonaws.com",
+		AccessKey: os.Getenv("AWS_ACCESS_KEY_ID"),
+		SecureKey: os.Getenv("AWS_SECRET_ACCESS_KEY"),
+	}
+
+	if err := s.Start(); err != nil {
+		t.Error(err)
 	}
 
 	err := tests.TestReadWriteDeleter(s, "s3_test_obj", "../../tests/test.jpg")
